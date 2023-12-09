fbcc75ff27139c0a44b7d44f0a2aadb409ff676a
JeanneJones <143661598+JeanneJones@users.noreply.github.com>
2023-12-08 20:29:28 -0700

    part 2 update

--- Assets/Animated Ch Move.cs
+++ Assets/Animated Ch Move.cs
@@ -13,78 +13,91 @@ public class AnimatedCharacterMovement : MonoBehaviour
 
     [Header("Jump")]
     public float jumpForce = 20f;
-    public float maxJumpHeight = 11f; // Adjust this value for your desired jump height.
+    public float maxJumpHeight = 11f;
 
     [Header("Ground Check")]
     public Transform groundCheck;
     public float groundCheckRadius = 0.1f;
     public LayerMask groundLayer;
 
-
-
+    private Transform originalParent;  // To store the original parent of the player
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        originalParent = transform.parent;  // Store the original parent
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Check if the player is grounded
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
-        // Reset triggers to default state
         anim.ResetTrigger("Jump");
         anim.ResetTrigger("Left");
         anim.ResetTrigger("Right");
-       
 
         float horizontalInput = Input.GetAxis("Horizontal");
-
-        // Calculate the new velocity based on the input
         Vector2 newVelocity = rb.velocity;
         newVelocity.x = horizontalInput * moveSpeed;
         rb.velocity = newVelocity;
 
         if (horizontalInput > 0f)
         {
-            // Right movement
             anim.SetTrigger("Right");
-            transform.localScale = new Vector3(1, 1, 1); // Flip sprite to face right
+            transform.localScale = new Vector3(1, 1, 1);
         }
-
         else if (horizontalInput < 0f)
         {
-            // Left movement
             anim.SetTrigger("Left");
-            transform.localScale = new Vector3(-1, 1, 1); // Flip sprite to face left
+            transform.localScale = new Vector3(-1, 1, 1);
         }
         else
         {
-            // Idle state when not moving horizontally
             anim.SetTrigger("Idle");
         }
 
-        // Handle jumping
         if (Input.GetKeyDown("space") && isGrounded)
         {
-            // Start jumping
             anim.SetTrigger("Jump");
             float actualJumpForce = Mathf.Sqrt(2f * jumpForce * Mathf.Abs(Physics2D.gravity.y) * maxJumpHeight);
             rb.velocity = new Vector2(rb.velocity.x, actualJumpForce);
+
+            // Unparent the player when they jump
+            transform.parent = originalParent;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Reset jumping state when landing on the ground
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            // Reassign the original parent when landing on the ground
+            transform.parent = originalParent;
+        }
+
+        // Check if colliding with a moving platform
+        if (collision.gameObject.CompareTag("MovingPlatform"))
+        {
+            // Parent the player to the moving platform
+            transform.parent = collision.transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+
+        // Unparent the player if they leave the moving platform
+        if (collision.gameObject.CompareTag("MovingPlatform"))
+        {
+            transform.parent = originalParent;
         }
     }
 }
-
---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+
+
+public class MovingPlatform : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Make the player a child of the moving platform
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Remove the player from being a child of the moving platform
+            collision.transform.SetParent(null);
+        }
+    }
+}
